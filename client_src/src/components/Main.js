0208f02d60593f6509c12154d625c4db3a7d5d50
Gregory Lesky <sleepydistrict01@gmail.com>
2017-12-02 12:35:21 -0700

    Restart admin features

--- client_src/src/components/Main.js
+++ client_src/src/components/Main.js
@@ -3,9 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Classes from './Classes';
 import Admin from './Admin';
 import Clients from './Clients';
-import ClassEdit from './ClassEdit';
 import AddClient from './AddClient';
-import ClassId from './ClassId';
 
 const Main = () => (
   <main>
@@ -13,9 +11,7 @@ const Main = () => (
         <Route exact path='/' component={Classes} />
         <Route exact path='/admin' component={Admin} />
         <Route exact path='/clients' component={Clients} />
-        <Route exact path='/classes/edit' component={ClassEdit} />
         <Route exact path='/clients/add' component={AddClient} />
-        <Route exact path='/classes/id:' component={ClassId} />
     </Switch>
   </main>    
 )
